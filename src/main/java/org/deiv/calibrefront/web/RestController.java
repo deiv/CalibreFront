16c3af83340aa8e05fa4981e524b6b73d38502bf
David Suárez <david.sephirot@gmail.com>
2019-02-17 16:46:49 +0100

    Add license info

--- src/main/java/org/deiv/calibrefront/web/RestController.java
+++ src/main/java/org/deiv/calibrefront/web/RestController.java
@@ -1,3 +1,31 @@
+/*
+ * @file RestController.java
+ *
+ * @brief Rest controller for books
+ * @author David Suárez
+ * @date Sun, 17 Feb 2019 15:26:01 +0100
+ *
+ * @license
+ *
+ * calibre-front: web front for calibre ebook manager.
+ *
+ * Copyright (C) 2019 <David Suárez <david.sephirot@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.deiv.calibrefront.web;
 
 import org.deiv.calibrefront.domain.dto.Book;
