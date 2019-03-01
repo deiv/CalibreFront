6a66b3eafe0c7a0f9f1a95d3052101503e214a58
David Suárez <david.sephirot@gmail.com>
2019-03-01 23:30:38 +0100

    Remove "custom column" table join from all books query

--- src/main/java/org/deiv/calibrefront/domain/mapper/BookMapper.java
+++ src/main/java/org/deiv/calibrefront/domain/mapper/BookMapper.java
@@ -38,15 +38,10 @@ import java.util.List;
 @Mapper
 public interface  BookMapper {
 
-    @Select("select book.*, paths.value as genre_path, author.authorsconcat as authors, \n" +
+    @Select("select book.*, author.authorsconcat as authors, \n" +
                 "            data.uncompressed_size as size, data.format,\n" +
                 "            rating.rating, comments.text as comments " +
                 "from books book \n" +
-                "  left join \n" +
-                "    (select path_value.value, book_path.book \n" +
-                "      from books_custom_column_1_link book_path \n" +
-                "      inner join custom_column_1 path_value on path_value.id = book_path.value) paths \n" +
-                "     on paths.book = book.id \n" +
                 "  left join _mcs_authors_concatenate author on author.book = book.id \n" +
                 "  left join data on data.book = book.id\n" +
                 "  left join\n" +
@@ -55,7 +50,7 @@ public interface  BookMapper {
                 "      inner join ratings on ratings.id = rating_link.rating) rating\n" +
                 "    on rating.book = book.id\n" +
                 "  left join comments on comments.book = book.id " +
-                "order by paths.value asc, book.title asc")
+                "order by book.title asc")
     List<Book> findAll();
 
     @Select("select path from books book where book.id = #{id} and has_cover = 1")
